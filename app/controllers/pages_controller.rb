351e2c3c50c81b8af063547890ddf3494e3cf10d
josh <josh.kp.jordan@gmail.com>
2021-08-09 10:58:44 +1200

    header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,20 @@ class PagesController < ApplicationController
 
     def privacy
     end
+
+    def success 
+    end
+
+    def fail 
+    end
+
+    def create
+        @contact = Contact.new(:name => params["name"], :email => params["email"], :phone => params["phone"], :message => params["message"])
+        
+        if @contact.save
+            redirect_to pages_success_path
+        else
+            redirect_to pages_fail_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get '/pages/privacy'
+  get 'pages/success'
+  get 'pages/fail'
+  resources :pages, only: [:create, :get]
 end
